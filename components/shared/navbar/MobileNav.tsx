1939280424a1170ce10f019e689e08cebb3d378a
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-07-04 00:08:39 +0700

    feat: implement mobile navigation hamburger menu

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,7 +1,112 @@
+"use client";
+
 import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetClose,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { SignedOut } from "@clerk/nextjs";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+
+const NavContent = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark-300_light900"} flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p
+                className={`${isActive ? "base-bold" : "base-medium text-dark100_light900"} `}
+              >
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
 
 const MobileNav = () => {
-  return <div>test</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="menu"
+          className="invert-colors md:hidden lg:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/devspace-icon.svg"
+            width={46}
+            height={46}
+            alt="DevSpace"
+            className="pb-1"
+          />
+          <p className="h2-bold font-mono text-watermelon-950 dark:text-watermelon-100">
+            DEV<span className="text-primary-500">SPACE</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Log In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="text-dark-400_light900 small-medium light-border-2 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
